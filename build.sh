c21c8592c5e14011e7cc5721c880dc7a9f272097
Nathanael Coonrod <ncoonrod@freelight.org>
2022-12-02 00:32:32 -0700

    chore: added test tag

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 NODE_VERSION=${1:-"16"}
 LATEST=${2:-"0"}
 HOTSTAQ_VERSION=${3:-$(cat ./package.json | jq -r .version)}
+TEMP_TAG=${4:-""}
 
 FULL_NAME="ourfreelight/hotstaq"
 
 # Build the debian slim images first
-TAG="$HOTSTAQ_VERSION-debian-slim-node$NODE_VERSION"
+TAG="$HOTSTAQ_VERSION-debian-slim-node$NODE_VERSION$TEMP_TAG"
 
 # Tag ex: hotstaq:0.6.31-debian-slim-node16
 docker build -t ${FULL_NAME}:${TAG} --build-arg="NODE_VERSION=$NODE_VERSION" --build-arg="HOTSTAQ_VERSION=$HOTSTAQ_VERSION" -f ./docker/amd64_debian.dockerfile .
