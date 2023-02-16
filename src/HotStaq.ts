2f3cf0f0674c18a2ace715698652ad1ffae265fd
Nathanael Coonrod <ncoonrod@freelight.org>
2023-02-16 11:57:47 -0700

    chore: added onPreRegister to generator

--- src/HotStaq.ts
+++ src/HotStaq.ts
@@ -131,7 +131,7 @@ export class HotStaq implements IHotStaq
 	/**
 	 * The current version of HotStaq.
 	 */
-	static version: string = "0.8.21";
+	static version: string = "0.8.22";
 	/**
 	 * Indicates if this is a web build.
 	 */
