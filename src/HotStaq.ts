f33458208ca31b5e053725a722b249c489cd9a69
Nathanael Coonrod <ncoonrod@freelight.org>
2023-02-20 22:01:24 -0700

    fix: websocket fixes;

--- src/HotStaq.ts
+++ src/HotStaq.ts
@@ -131,7 +131,7 @@ export class HotStaq implements IHotStaq
 	/**
 	 * The current version of HotStaq.
 	 */
-	static version: string = "0.8.23";
+	static version: string = "0.8.24";
 	/**
 	 * Indicates if this is a web build.
 	 */
