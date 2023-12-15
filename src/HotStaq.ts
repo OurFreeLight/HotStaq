65838766018387a067765273db01d06c9e93bd15
Nathanael Coonrod <ncoonrod@freelight.org>
2023-12-15 09:16:48 -0700

    fix: corrected incorrect method type being generated;

--- src/HotStaq.ts
+++ src/HotStaq.ts
@@ -132,7 +132,7 @@ export class HotStaq implements IHotStaq
 	/**
 	 * The current version of HotStaq.
 	 */
-	static version: string = "0.8.71";
+	static version: string = "0.8.72";
 	/**
 	 * Indicates if this is a web build.
 	 */
