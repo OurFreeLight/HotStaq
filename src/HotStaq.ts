3ea9fc76f9aa4f59dad8a2460c9a45c3ae7d1567
Nathanael Coonrod <ncoonrod@highersoftware.com>
2025-02-20 23:41:16 -0700

    0.8.105 merge (#138)
    * chore: added more cli support; chore: added better error code support;
    
    * fix: updated circleci;
    
    * fix: removed npm link;
    
    * feat: added better redis tls support;
    
    * feat: added swagger ui;

--- src/HotStaq.ts
+++ src/HotStaq.ts
@@ -132,7 +132,7 @@ export class HotStaq implements IHotStaq
 	/**
 	 * The current version of HotStaq.
 	 */
-	static version: string = "0.8.104";
+	static version: string = "0.8.105";
 	/**
 	 * Indicates if this is a web build.
 	 */
