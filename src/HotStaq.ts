dda3986e8496879cfb9ae5a28af0b4c87c4eea2c
Nathanael Coonrod <ncoonrod@freelight.org>
2025-03-08 23:57:37 -0700

    fix: corrected error message; (#148)

--- src/HotStaq.ts
+++ src/HotStaq.ts
@@ -172,7 +172,7 @@ export class HotStaq implements IHotStaq
 	/**
 	 * The current version of HotStaq.
 	 */
-	static version: string = "0.8.116";
+	static version: string = "0.8.117";
 	/**
 	 * Indicates if this is a web build.
 	 */
