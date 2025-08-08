f9467b35bc7c767d9164ee72b9620819844a1d17
ncoonrod <ncoonrod@freelight.org>
2025-08-07 22:29:15 -0700

    chore: version increment;

--- src/HotStaq.ts
+++ src/HotStaq.ts
@@ -211,7 +211,7 @@ export class HotStaq implements IHotStaq
 	/**
 	 * The current version of HotStaq.
 	 */
-	static version: string = "0.8.125";
+	static version: string = "0.8.126";
 	/**
 	 * Indicates if this is a web build.
 	 */
